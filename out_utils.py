874a337b70aa9d09848b6fe1b77c18a0f30dbd1f
philiplessner <philiplessner@gmail.com>
2016-04-12 22:21:57 +0000

    added decision boundary plotting function

--- out_utils.py
+++ out_utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from tabulate import tabulate
 
@@ -32,4 +33,21 @@ def print_predictions(y, yp):
     print(tabulate(list(zip(yp, y)),
                         headers=['yp', 'yi'],
                         tablefmt='fancy_grid'))
+
+
+# Helper function to plot a decision boundary.
+# If you don't fully understand this function don't worry, it just generates the contour plot below.
+def plot_decision_boundary(pred_func):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole gid
+    Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
 
